7fe857441ef02ecb86bcda8235b36bf6f864598e
liiishenggg <liiishenggg@163.com>
2020-04-02 11:34:49 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -22,8 +22,8 @@ module.exports = {
     },
     head: [["link", {rel: "icon", href: `/favicon.ico`}]],
     themeConfig: {
-        repo: "iiishenggg/newBook",
-        docsRepo: "iiishenggg/newBook",
+        repo: "iiishenggg/liiishenggg.github.io",
+        docsRepo: "iiishenggg/liiishenggg.github.io",
         editLinks: true,
         locales: {
             "/": {
